b2a75285594c0997e62c7819c555e24fa0e27587
Nick Fulton <nickfulton98@gmail.com>
2019-12-19 23:45:09 -0700

    Initial Push

---
+++ src/frontend/Driver.java
@@ -0,0 +1,22 @@
+package frontend;
+
+/**
+ * This is the driver for the TTRL Customer Tracking Tool. This was created by Nick Fulton
+ * for use at the San Jose State University Bowling Center. All sourcecode and materials 
+ * belong to Nick Fulton. For further information please contact me at my email: nickfulton98@gmail.com
+ * 
+ * @author	Nick Fulton
+ */
+public class Driver 
+{
+	/**
+	 * The main (and only) method of this class. This is used to launch the TTRL app, and possibly
+	 * handle more information in the future.
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		
+	}
+}
